d0e386bffd48a510388706a814f83f1d4321b1b9
[504]brandon <97409501+504brandon@users.noreply.github.com>
2022-06-19 13:54:56 -0500

    made the tank bg more accurate

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -592,7 +592,7 @@ class PlayState extends MusicBeatState
 						  clouds.updateHitbox();
 						  add(clouds);
 
-						  skittles = new FlxSprite(0, 50);
+						  skittles = new FlxSprite(100, 50);
 		                  skittles.frames = Paths.getSparrowAtlas('tank/tankWatchtower');
 		                  skittles.animation.addByPrefix('idle', 'watchtower gradient color instance 1', 24, false);
 		                  skittles.antialiasing = true;
@@ -608,7 +608,7 @@ class PlayState extends MusicBeatState
 		                  add(ground);
 
 				//tankmen
-					tank0 = new FlxSprite(-500,650);
+					tank0 = new FlxSprite(-500, 650);
 					tank0.frames = Paths.getSparrowAtlas('tank/tank0');
 					tank0.animation.addByPrefix('idle', 'fg tankhead far right instance 1', 24, false);
 					tank0.scrollFactor.set(1.7, 1.5);
@@ -622,7 +622,7 @@ class PlayState extends MusicBeatState
 					
 
 
-					tank1 = new FlxSprite(-300,750);
+					tank1 = new FlxSprite(-300, 750);
 					tank1.frames = Paths.getSparrowAtlas('tank/tank1');
 					tank1.animation.addByPrefix('idle', 'fg tankhead 5 instance 1', 24, false);
 					tank1.scrollFactor.set(2.0, 0.2);
@@ -636,7 +636,7 @@ class PlayState extends MusicBeatState
 					
 
 
-					tank2 = new FlxSprite(450,940);
+					tank2 = new FlxSprite(450, 940);
 					tank2.frames = Paths.getSparrowAtlas('tank/tank2');
 					tank2.animation.addByPrefix('idle', 'foreground man 3 instance 1', 24, false);
 					tank2.scrollFactor.set(1.5, 1.5);
@@ -649,7 +649,7 @@ class PlayState extends MusicBeatState
 					
 
 
-					tank3 = new FlxSprite(1300,1200);
+					tank3 = new FlxSprite(1300, 1200);
 					tank3.frames = Paths.getSparrowAtlas('tank/tank3');
 					tank3.animation.addByPrefix('idle', 'fg tankhead 4 instance 1', 24, false);
 					tank3.scrollFactor.set(3.5, 2.5);
@@ -662,7 +662,7 @@ class PlayState extends MusicBeatState
 					
 
 
-					tank4 = new FlxSprite(1300,900);
+					tank4 = new FlxSprite(1300, 900);
 					tank4.frames = Paths.getSparrowAtlas('tank/tank4');
 					tank4.animation.addByPrefix('idle', 'fg tankman bobbin 3 instance 1', 24, false);
 					tank4.scrollFactor.set(1.5, 1.5);
@@ -674,7 +674,7 @@ class PlayState extends MusicBeatState
 					
 					
 
-					tank5 = new FlxSprite(1620,700);
+					tank5 = new FlxSprite(1620, 700);
 					tank5.frames = Paths.getSparrowAtlas('tank/tank5');
 					tank5.animation.addByPrefix('idle', 'fg tankhead far right instance 1', 24, false);
 					tank5.scrollFactor.set(1.5, 1.5);
