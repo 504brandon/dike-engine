975bc3f48e2335707044ff5983ba819fa1d2d3d3
InsKal <94728303+InsKal@users.noreply.github.com>
2022-06-26 14:28:17 -0400

    im just too dumb

--- source/ui/ModifiersMenu.hx
+++ source/ui/ModifiersMenu.hx
@@ -64,7 +64,7 @@ class ModifiersMenu extends Page
 		ModifiersCheck('upd', false);
 		ModifiersCheck('op', false);
 		ModifiersCheck('insane', false);
-		ModifierCheck('gd', false)
+		ModifierCheck('gd', false); //funny line
 		
 		if (!getPref('fps-counter'))
 			Lib.current.stage.removeChild(Main.fpsCounter);
