1893d5c3443ddeaafb455df100f1b18073402855
[504]brandon <97409501+504brandon@users.noreply.github.com>
2022-08-10 19:56:36 -0500

    fixed speakers

--- source/Character.hx
+++ source/Character.hx
@@ -148,7 +148,7 @@ class Character extends FlxSprite
 				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
 				animation.addByPrefix('scared', 'GF FEAR', 24, true);
 
-				loadOffsetFile(curCharacter);
+				loadOffsetFile('gf');
 
 				playAnim('danceRight');
 
